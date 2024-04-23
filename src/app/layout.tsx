3f86966bf2ff815a25643d8243a4a234d5e60193
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-04-23 23:57:25 +0300

    Update layout.tsx to add Footer and BackgroundBeams components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,22 @@ import NavMenu from "@/app/_components/navMenu";
 import ScrollButton from "@/components/scrollButton";
 import SideMenu from "./_components/sideMenu";
 import MusicPlayer from "./_components/musicPlayer";
+import { headers } from "next/headers";
+import Footer from "./_components/footer";
+import { BackgroundBeams } from "@/components/background-beams";
 
 export const metadata: Metadata = {
   title: "E-Learning",
   description: "E-Learning platform for everyone to learn English by playing games for all levels.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type Props = Readonly<{
   children: React.ReactNode;
-}>) {
+}>;
+
+export default function RootLayout({ children }: Props) {
+  const headersList = headers()
+  const isMobile = headersList.get("user-agent")?.includes("Mobile")
   return (
     <html lang="en">
       <body className={`${manrope.variable} ${neonderthaw.variable} ${nekst.variable} antialiased`}>
@@ -29,11 +34,13 @@ export default function RootLayout({
         >
           <NavMenu />
           <ScrollButton />
-          <SideMenu />
+          {!isMobile && <SideMenu />}
           <MusicPlayer />
           <main className="">
             {children}
           </main>
+          <Footer />
+          <BackgroundBeams />
           <Toaster richColors />
         </ThemeProvider>
       </body>
