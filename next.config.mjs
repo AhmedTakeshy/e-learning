881ad52b762f57d15cd3e4b785d8487cf6ade4f6
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-04-23 23:56:53 +0300

    Add remotePatterns configuration for images in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wordwallscreens.azureedge.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
