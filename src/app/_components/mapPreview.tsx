9ae287a5716b5aedd67f57c05121053ec2f1f861
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-04-26 00:29:36 +0300

    Update MapPreview component to include a world map text

--- src/app/_components/mapPreview.tsx
+++ src/app/_components/mapPreview.tsx
@@ -14,25 +14,30 @@ export default function MapPreview() {
     const isMobile = (typeof window !== "undefined" && window.navigator.userAgent.includes("Mobile")) || window.innerWidth < 768;
 
     return (
-        <Map
-            mapboxAccessToken={mapboxToken}
-            {...viewState}
-            style={{
-                width: isMobile ? "23rem" : "52rem",
-                height: `${isMobile ? "25rem" : "38rem"}`,
-                borderRadius: "1rem",
-                padding: "2rem",
-                margin: "2rem",
-                display: "block",
-            }}
-            onZoom={(e) => setViewState(e.viewState)}
-            onMove={(e) => setViewState(e.viewState)}
-            mapStyle="mapbox://styles/ahmedtakeshy/clu4jdg9z00sz01qs5z222bu1"
-        >
-            <NavigationControl />
-            <GeolocateControl />
-            <FullscreenControl />
-        </Map>
+        <>
+            <Map
+                mapboxAccessToken={mapboxToken}
+                {...viewState}
+                style={{
+                    width: isMobile ? "23rem" : "52rem",
+                    height: `${isMobile ? "25rem" : "38rem"}`,
+                    borderRadius: "1rem",
+                    padding: "2rem",
+                    margin: "2rem",
+                    display: "block",
+                }}
+                onZoom={(e) => setViewState(e.viewState)}
+                onMove={(e) => setViewState(e.viewState)}
+                mapStyle="mapbox://styles/ahmedtakeshy/clu4jdg9z00sz01qs5z222bu1"
+            >
+                <NavigationControl />
+                <GeolocateControl />
+                <FullscreenControl />
+            </Map>
+            <p className="text-center md:text-lg">
+                Here is a world map where you can see continents, countries, and cities all over the world.
+            </p>
+        </>
 
     )
 }
